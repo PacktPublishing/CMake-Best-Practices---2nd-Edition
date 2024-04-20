d7f3fcb6a6f1e4c623029937f9044cde07f4bb47
Dominik Berner <dominik.berner@gmail.com>
2024-04-20 13:03:19 +0000

    Remove openssl deprecation warnings

--- chapter05/find_package_example/src/main.cpp
+++ chapter05/find_package_example/src/main.cpp
@@ -1,24 +1,52 @@
-#include <openssl/sha.h>
-#include <string>
-#include <iostream>
 #include <iomanip>
+#include <iostream>
+#include <openssl/evp.h>
+#include <openssl/sha.h>
 #include <sstream>
+#include <string>
 
-int main(int, char**)
-{
-    unsigned char hash[SHA256_DIGEST_LENGTH];
-    const std::string message{"CMake is awesome!"};
-    SHA256_CTX sha256;
-    SHA256_Init(&sha256);
-    SHA256_Update(&sha256, message.c_str(), message.size());
-    SHA256_Final(hash, &sha256);
+std::string sha256(const std::string &str) {
+  unsigned char hash[SHA256_DIGEST_LENGTH];
+  EVP_MD_CTX *mdctx;
 
-    std::stringstream ss; 
-    for(const auto& c : hash)
-    {
-        ss << std::hex << std::setw(2) << std::setfill('0') << (int)c;
-    }
+  if (!(mdctx = EVP_MD_CTX_new())) {
+    // Handle error
+    return "";
+  }
 
-    std::cout << "The sha256 hash of the message '" << message << "' is:\n";
-    std::cout << "\t" << ss.str() << "\n";
+  if (1 != EVP_DigestInit_ex(mdctx, EVP_sha256(), NULL)) {
+    // Handle error
+    EVP_MD_CTX_free(mdctx);
+    return "";
+  }
+
+  if (1 != EVP_DigestUpdate(mdctx, str.c_str(), str.size())) {
+    // Handle error
+    EVP_MD_CTX_free(mdctx);
+    return "";
+  }
+
+  unsigned int hash_len;
+  if (1 != EVP_DigestFinal_ex(mdctx, hash, &hash_len)) {
+    // Handle error
+    EVP_MD_CTX_free(mdctx);
+    return "";
+  }
+
+  EVP_MD_CTX_free(mdctx);
+
+  std::stringstream ss;
+  for (int i = 0; i < SHA256_DIGEST_LENGTH; i++) {
+    ss << std::hex << std::setw(2) << std::setfill('0') << (int)hash[i];
+  }
+
+  return ss.str();
+}
+
+int main(int, char **) {
+
+  const std::string message{"CMake is awesome!"};
+
+  std::cout << "The sha256 hash of the message '" << message << "' is:\n";
+  std::cout << "\t" << sha256(message) << "\n";
 }
